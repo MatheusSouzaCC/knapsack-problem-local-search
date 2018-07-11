73f88ae474d5fb7a35b53d1ea5e8ad7642fd919f
Matheus Souza <matheussouza@edu.univali.br>
2018-07-10 21:38:16 -0300

    Implementado busca local monótona

--- buscaLocal.js
+++ buscaLocal.js
@@ -1,57 +1,70 @@
 function executarBuscaLocal(instancia) {
     var t0 = performance.now();
-    alterarStatus('Executando o algoritmo');
-    var solucao = pegarSolucaoAleatoria(instancia);
-
+    alterarStatus('Executando o algoritmo');    
+    var solucaoInicial = pegarSolucaoAleatoria(instancia);
     alterarStatus('Algoritmo Executado');
     return {
-        solucao: solucao,
+        solucao: buscar(solucaoInicial,instancia),
         qtdItens: instancia.itens.length,
         tempoDecorrido: performance.now() - t0
     }
 }
 
-//solucao totalmente aleatoria ruim, de no maximo 10 itens
 function pegarSolucaoAleatoria(instancia) {
-    var rand = parseInt(Math.random() * 10);
     var s = {
         itens: [],
         pesoTotal: 0,
         valorTotal: 0
     };
+    let index = parseInt(Math.random() * 100);
+    for (index; index < instancia.itens.length; index++) {
 
-    for (let i = 0; i < rand; i++) {
-        let index = parseInt(Math.random() * 10);
         let item = instancia.itens[index];
-        s.itens.push(item);
-        s.pesoTotal += item.peso;
-        s.valorTotal += item.valor;
+        if (s.pesoTotal + item.peso <= instancia.capacidadeMochila) {
+            s.itens.push(item);
+            s.pesoTotal += item.peso;
+            s.valorTotal += item.valor;
+        } else {
+            return s;
+        }
+
     }
-
-    if (s.pesoTotal == 0 || s.pesoTotal > instancia.capacidadeMochila)
-        return pegarSolucaoAleatoria(instancia);
-
     return s;
 }
 
-//solucao aleatoria que tenta preencher a mochila
-//geralmente resulta em uma solução boa que provavelmente ja é um otimo local
-// function pegarSolucaoAleatoria(instancia) {
-//     var s = {
-//       itens: [],
-//       pesoTotal: 0,
-//       valorTotal: 0
-//     };
-//     var pesoTotalAux = 0;
-//     while (pesoTotalAux < instancia.capacidadeMochila) {
-//       let index = parseInt(Math.random() * 10);
-//       let item = instancia.itens[index];
-//       if (s.pesoTotal + item.peso <= instancia.capacidadeMochila) {
-//         s.itens.push(item);
-//         s.pesoTotal += item.peso;
-//         s.valorTotal += item.valor;
-//       }
-//       pesoTotalAux += item.peso;
-//     }
-//     return s;
-//   }
+function buscar(solucao, instancia) {
+    var novaSolucao = JSON.parse(JSON.stringify(solucao));
+    var index = parseInt(Math.random() * (solucao.itens.length - 1));
+    var itemTroca = solucao.itens[index];
+
+    for (let i = 0; i < instancia.itens.length; i++) {
+        var itemAtual = instancia.itens[i];
+        
+        if(itemAtual.valor > itemTroca.valor){
+            if(!verificarExistencia(itemAtual, novaSolucao.itens)){
+                if((novaSolucao.pesoTotal - itemTroca.peso) + itemAtual.peso  < instancia.capacidadeMochila){
+                    novaSolucao.itens[index] = itemAtual;
+                    novaSolucao.pesoTotal = novaSolucao.pesoTotal - itemTroca.peso + itemAtual.peso;
+                    novaSolucao.valorTotal = novaSolucao.valorTotal - itemTroca.valor + itemAtual.valor;
+                    
+                    if(novaSolucao.valorTotal >= solucao.valorTotal)
+                        return buscar(novaSolucao, instancia);
+                    return solucao;
+                }
+            }
+        }
+    }
+
+    if (novaSolucao.valorTotal > solucao.valorTotal)
+        return buscar(novaSolucao, itens);
+
+    return solucao;
+}
+
+function verificarExistencia(item, itens) {
+    for (let i = 0; i < itens.length; i++) {
+        if (itens[i].peso == item.peso && itens[i].valor == item.valor)
+            return true;
+    }
+    return false;
+}
